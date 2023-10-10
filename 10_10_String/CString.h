91d3f7e519349d99ad5786a2fe1697948a2f2cbf
park se won <tpdnjsdkwk@naver.com>
2023-10-10 23:14:04 +0900

    docs

--- 10_10_String/CString.h
+++ 10_10_String/CString.h
@@ -9,10 +9,11 @@ public:
 	CString():len(0),cName(nullptr) {};
 
 	CString(const char* _Name)
-	{len=strlen(_Name)+1;
-	 cName=new char[len];
-	 cName=_Name;
-		//cName = const_cast<char*>(_Name);
+	{
+		len=strlen(_Name)+1;
+		 cName=new char[len];
+		//cName=_Name;
+		cName = const_cast<char*>(_Name);
 
 	}
 
@@ -25,21 +26,28 @@ public:
 
 	~CString()
 	{
-		cout << "¼Ò¸êÀÚ" << endl;
-	};
+		if (cName != nullptr)
+		{
+			delete[]cName;
+			cName = nullptr;
+		}
+		//cout <<"소멸자" << endl;
+	}
 
 	CString& operator=(const CString& _Name)
 	{
 		//cName = _Name.cName;
-		strcpy_s(cName, sizeof(cName) / sizeof(char), _Name.cName);
+		const int len = strlen(_Name.cName) + 1;
+		cName = new char[len];
+		strcpy_s(cName, sizeof(len) ,_Name.cName);
 		return *this;
 
 	}
 	CString& operator=(const char* _Name)
 	{
-		len = strlen(_Name) + 1;
+		int len = strlen(_Name) + 1;
 		cName = new char[len];
-		strcpy_s(cName, sizeof(cName)/sizeof(char), _Name);
+		strcpy_s(cName, sizeof(cName),_Name);
 		return *this;
 	}
 
