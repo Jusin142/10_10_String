5ce0b598e6176d23bdf97b0950791bbdc10047a4
park se won <tpdnjsdkwk@naver.com>
2023-10-11 00:36:04 +0900

    docs

--- 10_10_String/10_10_String.cpp
+++ 10_10_String/10_10_String.cpp
@@ -10,13 +10,18 @@ int main()
 {
 	CString name = "1";
 	CString test = "22";
+	CString car = test;
+	CString Man(name);
 
 	test = "333";
 	name = test;
+	Man = "123456";
+	car = name;
 	//test = "444";
 	name = "ppppp";
 	
-
+	cout << Man << endl;
+	cout << car << endl;
 	cout << name << endl;
 	cout << test << endl;
 
